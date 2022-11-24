3dfbb09a96dcbdf67653a2c4f3b371ff0117b033
yiladien@hotmail.com <yiladien@hotmail.com>
2022-11-24 04:39:06 -0600

    added IndexedDB

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -1,6 +1,8 @@
 // js files
 import "./form";
 import "./submit";
+import "./database";
+import { initDb, getDb, postDb } from "./database";
 
 // css
 import { Tooltip, Toast, Popover } from "bootstrap";
@@ -11,8 +13,15 @@ import "../css/index.css";
 import Logo from "../images/logo.png";
 import Bear from "../images/bear.png";
 import Dog from "../images/dog.png";
+import { initdb } from "./database";
 
 window.addEventListener("load", function () {
+  initdb();
+  // We are temporarily placing getDb() and postDb() function calls here for testing. We will move it to another event listener later.
+  getDb();
+  postDb("Lernantino", "learnantino@test.com", 8186601234, "Bear");
+  getDb();
+
   document.getElementById("logo").src = Logo;
   document.getElementById("bearThumbnail").src = Bear;
   document.getElementById("dogThumbnail").src = Dog;
