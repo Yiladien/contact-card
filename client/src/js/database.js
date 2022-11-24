b01946ada2e8a155e72fdeea00f2c60dbb827a0f
yiladien@hotmail.com <yiladien@hotmail.com>
2022-11-24 13:06:17 -0600

    updated edit feature

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,7 +1,7 @@
 import { openDB } from "idb";
 import "regenerator-runtime/runtime";
 
-export const initdb = async () => {
+export const initDb = async () => {
   // We are creating a new database named 'contact_db' which will be using version 1 of the database.
   openDB("contact_db", 1, {
     // Add our database schema if it has not already been initialized.
@@ -54,6 +54,52 @@ export const postDb = async (name, email, phone, profile) => {
 
   // Use the .add() method on the store and pass in the content.
   const request = store.add({
+    name: name,
+    email: email,
+    phone: phone,
+    profile: profile,
+  });
+
+  // Get confirmation of the request.
+  const result = await request;
+  console.log("🚀 - data saved to the database", result);
+};
+
+export const deleteDb = async (id) => {
+  console.log("DELETE from the database", id);
+
+  // Create a connection to the IndexedDB database and the version we want to use.
+  const contactDb = await openDB("contact_db", 1);
+
+  // Create a new transaction and specify the store and data privileges.
+  const tx = contactDb.transaction("contacts", "readwrite");
+
+  // Open up the desired object store.
+  const store = tx.objectStore("contacts");
+
+  // Use the .delete() method to get all data in the database.
+  const request = store.delete(id);
+
+  // Get confirmation of the request.
+  const result = await request;
+  console.log("result.value", result);
+  return result?.value;
+};
+
+export const editDb = async (name, email, phone, profile) => {
+  console.log("PUT to the database");
+
+  // Create a connection to the database and specify the version we want to use.
+  const contactDb = await openDB("contact_db", 1);
+
+  // Create a new transaction and specify the store and data privileges.
+  const tx = contactDb.transaction("contacts", "readwrite");
+
+  // Open up the desired object store.
+  const store = tx.objectStore("contacts");
+
+  // Use the .add() method on the store and pass in the content.
+  const request = store.put({
     name: name,
     email: email,
     phone: phone,
