459a81c6a9aa8ca309c1c0771e27b7d31eff8634
yiladien@hotmail.com <yiladien@hotmail.com>
2022-11-23 22:47:30 -0600

    added heroku local web

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -1,6 +1,13 @@
+// js files
 import "./form";
 import "./submit";
+
+// css
+import { Tooltip, Toast, Popover } from "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "../css/index.css";
+
+// images
 import Logo from "../images/logo.png";
 import Bear from "../images/bear.png";
 import Dog from "../images/dog.png";
