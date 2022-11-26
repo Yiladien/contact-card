c0b941a8ef7a16d9ee2e02354276cc945b23c7f8
yiladien@hotmail.com <yiladien@hotmail.com>
2022-11-26 11:06:16 -0600

    added install feature

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -21,6 +21,23 @@ if ("serviceWorker" in navigator) {
   });
 }
 
+const installBtn = document.getElementById("installBtn");
+
+window.addEventListener("beforeinstallprompt", (event) => {
+  event.preventDefault();
+  installBtn.style.visibility = "visible";
+
+  installBtn.addEventListener("click", () => {
+    event.prompt();
+    installBtn.setAttribute("disabled", true);
+    installBtn.textContent = "Installed!";
+  });
+});
+
+window.addEventListener("appinstalled", (event) => {
+  console.log("👍", "appinstalled", event);
+});
+
 window.addEventListener("load", function () {
   initDb();
 
